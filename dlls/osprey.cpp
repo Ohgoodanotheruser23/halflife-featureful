1f09036646fa42c6b271a9578b803305f4b81b3d
Roman Chistokhodov <freeslave93@gmail.com>
2018-08-23 00:59:23 +0300

    Fix monsters on moving platforms

--- dlls/osprey.cpp
+++ dlls/osprey.cpp
@@ -151,7 +151,7 @@ void COsprey::Spawn( void )
 	UTIL_SetSize( pev, Vector( -400, -400, -100 ), Vector( 400, 400, 32 ) );
 	UTIL_SetOrigin( pev, pev->origin );
 
-	pev->flags |= FL_MONSTER;
+	pev->flags |= FL_MONSTER | FL_FLY;
 	pev->takedamage = DAMAGE_YES;
 	m_flRightHealth = 200;
 	m_flLeftHealth = 200;
