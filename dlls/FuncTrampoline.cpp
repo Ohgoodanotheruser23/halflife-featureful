c2dad633bcf5aefd0b71a8a1e497b30ef9ec0f75
Ohgoodanotheruser23 <50934667+Ohgoodanotheruser23@users.noreply.github.com>
2024-08-30 15:56:22 -0700

    bool caused gleetch

--- dlls/FuncTrampoline.cpp
+++ dlls/FuncTrampoline.cpp
@@ -25,15 +25,13 @@ class CFuncTrampoline : public CBaseEntity
 	static TYPEDESCRIPTION m_SaveData[];
 
 	int m_iCanExplode = 0;
-	bool bCanExplode = false;
 };
 
 LINK_ENTITY_TO_CLASS(func_trampoline, CFuncTrampoline);
 
 TYPEDESCRIPTION CFuncTrampoline::m_SaveData[] =
 {
-	DEFINE_FIELD(CFuncTrampoline, m_iCanExplode, FIELD_INTEGER),
-	DEFINE_FIELD(CFuncTrampoline, bCanExplode, FIELD_BOOLEAN) };
+	DEFINE_FIELD(CFuncTrampoline, m_iCanExplode, FIELD_INTEGER) };
 
 IMPLEMENT_SAVERESTORE(CFuncTrampoline, CBaseEntity);
 
@@ -46,7 +44,7 @@ void CFuncTrampoline::Spawn()
 
 void CFuncTrampoline::Touch(CBaseEntity* pOther)
 {
-	if (bCanExplode)
+	if (m_iCanExplode == 1)
 	{
 		pOther->pev->velocity.z += 400.0f;
 
@@ -62,8 +60,6 @@ void CFuncTrampoline::KeyValue(KeyValueData* pkvd)
 	if (FStrEq(pkvd->szKeyName, "canExplode"))
 	{
 		m_iCanExplode = atoi(pkvd->szValue);
-		bCanExplode = true;
-
 		pkvd->fHandled = true;
 	}
 	else
