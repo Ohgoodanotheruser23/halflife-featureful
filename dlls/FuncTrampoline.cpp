c7955a9cb23af82242df07a7769b44d43b01b38b
Ohgoodanotheruser23 <50934667+Ohgoodanotheruser23@users.noreply.github.com>
2024-08-30 14:33:54 -0700

    Oldies

---
+++ dlls/FuncTrampoline.cpp
@@ -0,0 +1,73 @@
+/*
+================================================================
+	Func_Trampoline
+		Boing
+================================================================
+*/
+
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "explode.h"
+
+// TODO: Add keyvalues for explosions
+
+class CFuncTrampoline : public CBaseEntity
+{
+	void Spawn() override;
+	void Touch(CBaseEntity* pOther) override;
+
+	void KeyValue(KeyValueData* pkvd) override;
+
+	int Save(CSave& save);
+	int Restore(CRestore& restore);
+
+	static TYPEDESCRIPTION m_SaveData[];
+
+	int m_iCanExplode = 0;
+	bool bCanExplode = false;
+};
+
+LINK_ENTITY_TO_CLASS(func_trampoline, CFuncTrampoline);
+
+TYPEDESCRIPTION CFuncTrampoline::m_SaveData[] =
+{
+	DEFINE_FIELD(CFuncTrampoline, m_iCanExplode, FIELD_INTEGER),
+	DEFINE_FIELD(CFuncTrampoline, bCanExplode, FIELD_BOOLEAN) };
+
+IMPLEMENT_SAVERESTORE(CFuncTrampoline, CBaseEntity);
+
+void CFuncTrampoline::Spawn()
+{
+	pev->movetype = MOVETYPE_NONE;
+	pev->solid = SOLID_TRIGGER;
+	SET_MODEL(edict(), STRING(pev->model));
+}
+
+void CFuncTrampoline::Touch(CBaseEntity* pOther)
+{
+	if (bCanExplode)
+	{
+		pOther->pev->velocity.z += 400.0f;
+
+		Vector position = pOther->pev->origin + (pOther->pev->maxs + pOther->pev->mins) * 0.5f;
+		ExplosionCreate(position, pev->angles, edict(), 50, true);
+	}
+	else
+		pOther->pev->velocity.z += 400.0f;
+}
+
+void CFuncTrampoline::KeyValue(KeyValueData* pkvd)
+{
+	if (FStrEq(pkvd->szKeyName, "canExplode"))
+	{
+		m_iCanExplode = atoi(pkvd->szValue);
+		bCanExplode = true;
+
+		pkvd->fHandled = true;
+	}
+	else
+	{
+		return CBaseEntity::KeyValue(pkvd);
+	}
+}
